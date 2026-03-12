660f04a9739d3ac7e54a9e96d3ec76e4a947eb00
Juan Manuel Truffa <jmtruffa@gmail.com>
2026-03-12 13:09:06 -0300

    Update arranca_crud.sh to dynamically retrieve server IP address for app URL

--- arranca_crud.sh
+++ arranca_crud.sh
@@ -4,4 +4,12 @@
 cd ~/dev/crud_bonds/server
 npm start &
 
-echo "App corriendo en http://34.44.118.200:4000"
+DEFAULT_IFACE=$(route -n get default 2>/dev/null | awk '/interface:/{print $2}')
+SERVER_IP=$(
+  ipconfig getifaddr "$DEFAULT_IFACE" 2>/dev/null || \
+  ipconfig getifaddr en0 2>/dev/null || \
+  ipconfig getifaddr en1 2>/dev/null || \
+  echo "localhost"
+)
+
+echo "App corriendo en http://$SERVER_IP:4000"
